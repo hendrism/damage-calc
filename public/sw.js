d52e37ed1a1ba9e0579c011c542aa1b0544a8313
hendrism <hendrism@gmail.com>
2025-07-30 11:08:48 -0400

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,20 +1,15 @@
-diff --git a//dev/null b/public/sw.js
-index 0000000000000000000000000000000000000000..f0ef6aea35e55772acf98eccf83cd74b17961f48 100644
---- a//dev/null
-+++ b/public/sw.js
-@@ -0,0 +1,15 @@
-+self.addEventListener('install', event => {
-+  event.waitUntil(
-+    caches.open('damage-calc-v1').then(cache => cache.addAll([
-+      '/',
-+      '/index.html',
-+      '/manifest.webmanifest'
-+    ]))
-+  );
-+});
-+
-+self.addEventListener('fetch', event => {
-+  event.respondWith(
-+    caches.match(event.request).then(response => response || fetch(event.request))
-+  );
-+});
+self.addEventListener('install', event => {
+   event.waitUntil(
+     caches.open('damage-calc-v1').then(cache => cache.addAll([
+       '/',
+       '/index.html',
+       '/manifest.webmanifest'
+     ]))
+   );
+ });
+
+ self.addEventListener('fetch', event => {
+   event.respondWith(
+     caches.match(event.request).then(response => response || fetch(event.request))
+   );
+ });
