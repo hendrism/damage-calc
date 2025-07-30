f9af11c2202a73cd3ee323b38e89e1e4067ee683
hendrism <hendrism@gmail.com>
2025-07-30 11:15:44 -0400

    Improve PWA implementation

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,43 @@
-self.addEventListener('install', event => {
-   event.waitUntil(
-     caches.open('damage-calc-v1').then(cache => cache.addAll([
-       '/',
-       '/index.html',
-       '/manifest.webmanifest'
-     ]))
-   );
- });
+const CACHE_NAME = 'damage-calc-v1';
+const OFFLINE_URLS = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
 
- self.addEventListener('fetch', event => {
-   event.respondWith(
-     caches.match(event.request).then(response => response || fetch(event.request))
-   );
- });
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_URLS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request)
+        .then(response => {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseClone);
+          });
+          return response;
+        })
+        .catch(() => caches.match('/index.html'));
+    })
+  );
+});
