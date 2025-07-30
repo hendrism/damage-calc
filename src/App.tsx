722deb648b3dedfdf2f7e861610496921eaeea43
hendrism <hendrism@gmail.com>
2025-07-30 12:01:50 -0400

    feat: enable kid mode toggle everywhere and add simple text

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,5 @@ export default function App() {
     document.body.classList.toggle('kid-mode', kidMode);
   }, [kidMode]);
 
-  return (
-    <div>
-      <button
-        onClick={() => setKidMode(!kidMode)}
-        className="fixed top-4 right-4 bg-white/80 text-gray-800 px-4 py-2 rounded-lg shadow-lg hover:bg-white"
-      >
-        {kidMode ? 'Adult Mode' : 'Kid Mode'}
-      </button>
-      <CardDamageCalculator />
-    </div>
-  );
+  return <CardDamageCalculator kidMode={kidMode} setKidMode={setKidMode} />;
 }
